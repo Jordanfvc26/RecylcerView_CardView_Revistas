dd4ec2dcd1cd6856ca3d2f0469c44d707b8b754d
Jordan Vera <jverac12@uteq.edu.ec>
2022-07-12 02:08:03 -0500

    Initial commit

---
+++ app/src/main/java/com/example/recylcerview_cardview_revistas/Revistas.kt
@@ -0,0 +1,11 @@
+package com.example.recylcerview_cardview_revistas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Revistas : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_revistas)
+    }
+}
